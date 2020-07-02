c21f863434f7b5c33dc6c221c2b549ad82436a0c
Wei Li <wei.li@redhat.com>
2020-07-02 11:03:36 +0100

    make the app work for RHMI 1.x

--- deploy/undeploy.sh
+++ deploy/undeploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 NS=${NAMESPACE:-"workload-web-app"}
+AMQONLINE_NS=${AMQONLINE_NAMESPACE:-"redhat-rhmi-amq-online"}
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Clean the Workload App
@@ -9,7 +10,12 @@ oc delete all -l app=workload-web-app -n $NS
 # Clean AMQ Resources
 oc delete address/workload-app.queue-requests -n $NS
 oc delete addressspace/workload-app -n $NS
-oc delete authenticationservice/none-authservice -n $NS
+if [[ ! -z "${RHMI_V1}" ]]; then
+  oc delete authenticationservice/none-authservice -n $AMQONLINE_NS
+else
+  oc delete authenticationservice/none-authservice -n $NS
+fi
+
 oc delete project $NS
 
 # Clean 3scale Resources
